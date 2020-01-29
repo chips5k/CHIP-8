193f7a3a7f0900ed885f7b35452722251dc96985
Callem Pittard <callem.pittard@gmail.com>
2020-01-29 21:28:22 +1100

    the possibilities!

---
+++ render-test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+echo "|"
+echo "|"
+echo "|"
+echo "|"
+sleep 0.3
+echo -en "\e[2A"
+echo " |"
+echo " |"
+echo " |"
+echo " |"
+sleep 0.3
+echo -en "\e[2A"
+echo "  |"
+echo "  |"
+echo "  |"
+sleep 0.3
+echo -en "\e[2A"
+echo "  |"
+echo "  |"
+echo "  |"
+echo "  |"
+
+sleep 0.3
+echo -en "\e[2A"
+echo "   |"
+echo "   |"
+echo "   |"
+echo "   |"
+
+sleep 0.3
+echo -en "\e[2A"
+echo "    |"
+echo "    |"
+echo "    |"
+echo "    |"
+
+sleep 0.3
+echo -en "\e[2A"
+echo "     |"
+echo "     |"
+echo "     |"
+echo "     |"
+
+
